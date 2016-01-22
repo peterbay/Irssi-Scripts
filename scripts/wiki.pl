cc87aae8cde1c184252db982c998ff0597b7f8e8
peterbay <pb.pb@centrum.cz>
2016-01-22 09:50:39 +0100

    reformated code

--- scripts/wiki.pl
+++ scripts/wiki.pl
@@ -6,6 +6,7 @@ use Irssi::Irc;
 use LWP::UserAgent;
 use JSON qw( decode_json );
 use Text::Iconv;
+use URI::Escape;
 
 use vars qw($VERSION %IRSSI);
 
@@ -30,46 +31,42 @@ sub on_public {
 	my ( $searchTerm ) = ( $1 );
 	
 	my $ua       = LWP::UserAgent->new ( ssl_opts => { verify_hostname => 0 } );
-  my $response = $ua->get ( "https://cs.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" . &uri_escape ( $searchTerm ) );
+	my $response = $ua->get ( "https://cs.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" . &uri_escape ( $searchTerm ) );
 
-		if ( $response->is_success ) {
-			my $xmlString  = $response->decoded_content;
-      
- my $json = decode_json( $jsonString );
-      
-      if ( defined ( $json->{ "query" } ) && defined ( $json->{ "query" }->{ "search" } ) ){
-      
-      		my $itemCounter = 0;
-		my $itemLimit   = 3;
-      
-        $server->send_message ( $dst, $nick . ": WIKI search - " . $searchTerm, 0 );
-      
-        foreach my $entry ( @{$json->{ "query" }->{ "search" }} ){
-      
-          my $title   = defined ( $entry->{ "title"   } ) ? $entry->{ "title" } : "";
-          my $snippet = defined ( $entry->{ "snippet" } ) ? $entry->{ "snippet" } : "";
-      
-          my $shortSnippet = $snippet;
-          $shortSnippet =~ s/\.\s\s*[A-Z].*$//g;
-          $shortSnippet =~ s/<[^>+>]+>//g;
-      
-          if ( length ( $shortSnippet ) > 50 ){
-            $shortSnippet = substr ( $shortSnippet, 0, 60 ) . " ...";
-          }
+	if ( $response->is_success ) {
+		my $jsonString  = $response->decoded_content;
 
-          if ( $itemCounter < $itemLimit ){
-            $server->send_message ( $dst, $nick . ": WIKI - " . $converter->convert ( $title . " - " . $shortSnippet ), 0 );
-          
-          }
-        
-          $itemCounter++;
-        
-        }
-      
-      }
-		} else {
-			$server->send_message ( $dst, $nick . ": WIKI ERROR - " . $response->status_line, 0 );
+		my $json = decode_json( $jsonString );
+
+		if ( defined ( $json->{ "query" } ) && defined ( $json->{ "query" }->{ "search" } ) ){
+
+	 		my $itemCounter = 0;
+			my $itemLimit   = 5;
+
+			$server->send_message ( $dst, $nick . ": WIKI search - " . $searchTerm, 0 );
+
+			foreach my $entry ( @{$json->{ "query" }->{ "search" }} ){
+
+				my $title   = defined ( $entry->{ "title"   } ) ? $entry->{ "title" } : "";
+				my $snippet = defined ( $entry->{ "snippet" } ) ? $entry->{ "snippet" } : "";
+
+				my $shortSnippet = $snippet;
+				$shortSnippet =~ s/\.\s\s*[A-Z].*$//g;
+				$shortSnippet =~ s/<[^>+>]+>//g;
+
+				if ( length ( $shortSnippet ) > 50 ){
+					$shortSnippet = substr ( $shortSnippet, 0, 60 ) . " ...";
+				}
+
+				if ( $itemCounter < $itemLimit ){
+					$server->send_message ( $dst, $nick . ": WIKI - " . $converter->convert ( $title . " - " . $shortSnippet ), 0 );
+
+				}
+				$itemCounter++;
+			}
 		}
+	} else {
+		$server->send_message ( $dst, $nick . ": WIKI ERROR - " . $response->status_line, 0 );
 	}
 }
 
