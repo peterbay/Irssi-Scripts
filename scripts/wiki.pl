32d2835ff1a1eb4a86a993cbe7f7530931dcc140
peterbay <pb.pb@centrum.cz>
2016-01-22 09:33:46 +0100

    wiki init

---
+++ scripts/wiki.pl
@@ -0,0 +1,71 @@
+use strict;
+use warnings;
+
+use Irssi;
+use Irssi::Irc;
+use LWP::UserAgent;
+use JSON qw( decode_json );
+use Text::Iconv;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '0.01';
+%IRSSI = (
+	authors     => "Petr Vavrin",
+	contact     => "pb.pb\@centrum.cz",
+	name        => "Wiki",
+	description => "description",
+);
+
+sub on_public {
+	my ( $server, $message, $nick, $hostmask, $channel ) = @_;
+	my $cp = Irssi::settings_get_str ( 'bot_cmd_prefix' );
+	my $isprivate = !defined $channel;
+	my $dst = $isprivate ? $nick : $channel;
+
+	my $converter = Text::Iconv->new ( "utf-8", "windows-1250" );
+
+	return unless $message =~ /^${cp}wiki\s+(.*)$/;
+
+	my ( $searchTerm ) = ( $1 );
+	
+	my $ua       = LWP::UserAgent->new ( ssl_opts => { verify_hostname => 0 } );
+  my $response = $ua->get ( "https://cs.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=" . &uri_escape ( $searchTerm ) );
+
+		if ( $response->is_success ) {
+			my $xmlString  = $response->decoded_content;
+      
+ my $json = decode_json( $jsonString );
+      
+      if ( defined ( $json->{ "query" } ) && defined ( $json->{ "query" }->{ "search" } ) ){
+      
+        $server->send_message ( $dst, $nick . ": WIKI search - " . $searchTerm, 0 );
+      
+        foreach my $entry ( @{$json->{ "query" }->{ "search" }} ){
+      
+          my $title   = defined ( $entry->{ "title"   } ) ? $entry->{ "title" } : "";
+          my $snippet = defined ( $entry->{ "snippet" } ) ? $entry->{ "snippet" } : "";
+      
+          $shortSnippet = $snippet;
+          $shortSnippet =~ s/\.\s\s*[A-Z].*$//g;
+          $shortSnippet =~ s/<[^>+>]+>//g;
+      
+          if ( length ( $shortSnippet ) > 50 ){
+            $shortSnippet = substr ( $shortSnippet, 0, 60 ) . " ...";
+          }
+
+          $server->send_message ( $dst, $nick . ": WIKI - " . $converter->convert ( $title . " - " . $shortSnippet ), 0 );
+        
+        }
+      
+      }
+		} else {
+			$server->send_message ( $dst, $nick . ": WIKI ERROR - " . $response->status_line, 0 );
+		}
+	}
+}
+
+Irssi::signal_add('message public', 'on_public');
+Irssi::signal_add('message private', 'on_public');
+
+Irssi::settings_add_str('bot', 'bot_cmd_prefix', '`');
