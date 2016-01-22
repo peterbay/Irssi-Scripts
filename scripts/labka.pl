e4f9e2aabcb3d3d3f44e9f20f3e8dca3d2efcfcf
peterbay <pb.pb@centrum.cz>
2016-01-22 01:35:09 +0100

    update

--- scripts/labka.pl
+++ scripts/labka.pl
@@ -4,7 +4,7 @@ use warnings;
 use Irssi;
 use Irssi::Irc;
 use LWP::UserAgent;
-use XML::Feed;
+use XML::Simple;
 use Text::Iconv;
 
 use vars qw($VERSION %IRSSI);
@@ -23,42 +23,53 @@ sub on_public {
 	my $isprivate = !defined $channel;
 	my $dst = $isprivate ? $nick : $channel;
 
-  my $converter = Text::Iconv->new ( "utf-8", "windows-1250" );
+	my $converter = Text::Iconv->new ( "utf-8", "windows-1250" );
 
 	return unless $message =~ /^${cp}labka\s+(.*)$/;
 
 	my ( $command ) = ( $1 );
 	
-  for ( $command ){
-    s/^\s+|\s+$//g;
-    uc ( $_ );
-  }
-  
-  if ( $command eq "RSS" ){
-  
-    my $ua = LWP::UserAgent->new(ssl_opts => { verify_hostname => 0 });
-    my $response = $ua->get("https://labka.cz/wiki/feed.php");
-  
-    if ($response->is_success) {
-      my $xml = $response->decoded_content;  # or whatever
-     
-      my $feed = XML::Feed->parse(\$xml);
+	for ( $command ){
+		s/^\s+|\s+$//;
+	}
+	$command = uc ( $command );
 
-      foreach ($feed->entries) {
-      
-#         my $answerEncoded = $converter->convert ( $answer );
-      
-        $server->send_message ( $dst, $nick . ": RSS - " . $_->title, 0 );
-        #   print $_->content->body, "\n";
-      }
-     
-    } else {
-      $server->send_message ( $dst, $nick . ": ERROR - " . $response->status_line, 0 );
-    }
-  
-  }
+	if ( $command eq "RSS" ){
 
-	$server->send_message ( $dst, $nick . ": " . $answerEncoded, 0 );
+		my $ua       = LWP::UserAgent->new ( ssl_opts => { verify_hostname => 0 } );
+		my $response = $ua->get ( "https://labka.cz/wiki/feed.php" );
+
+		if ( $response->is_success ) {
+			my $xmlString  = $response->decoded_content;
+			my $xmlSimple  = XML::Simple->new( );
+			my $xmlData    = $xmlSimple->XMLin( $xmlString );
+
+			if ( defined ( $xmlData->{ "channel" } ) ){
+				my $channelTitle = defined ( $xmlData->{ "channel" }->{ "title" } ) ? $xmlData->{ "channel" }->{ "title" } : "";
+				$server->send_message ( $dst, $nick . ": RSS Channel - " . $converter->convert ( $channelTitle ), 0 );
+
+			}
+
+			if ( defined ( $xmlData->{ "item" } ) ){
+
+				 foreach my $item ( @{$xmlData->{ "item" }} ){
+
+					my $itemTitle = defined ( $item->{ "title" } ) ? $item->{ "title" } : "";
+					my $itemDate  = defined ( $item->{ "dc:date" } ) ? $item->{ "dc:date" } : "";
+
+					for ( $itemDate ){
+						s/\+.*$//g;
+						s/T/ /g;
+					}
+
+					$server->send_message ( $dst, $nick . ": RSS - " . $converter->convert ( $itemDate . " - " . $itemTitle ), 0 );
+
+				}
+			}
+		} else {
+			$server->send_message ( $dst, $nick . ": ERROR - " . $response->status_line, 0 );
+		}
+	}
 }
 
 Irssi::signal_add('message public', 'on_public');
